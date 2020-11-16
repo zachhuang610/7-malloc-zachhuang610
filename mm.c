e4331e6d5cad123f86754975299c6b5c6f10a2cd
zachhuang610 <zachary_huang@brown.edu>
2020-11-16 02:32:06 +0000

    mm_malloc error

--- mm.c
+++ mm.c
@@ -20,6 +20,11 @@
 #include "./mm.h"
 #include "./mminline.h"
 
+#define EXTENSION (2 * MINBLOCKSIZE)
+static block_t* flist_first;
+block_t *prol;
+block_t *epil;
+
 // rounds up to the nearest multiple of WORD_SIZE
 static inline size_t align(size_t size) {
   return (((size) + (WORD_SIZE - 1)) & ~(WORD_SIZE - 1));
@@ -38,7 +43,73 @@ static inline size_t align(size_t size) {
  * returns: 0, if successful
  *         -1, if an error occurs
  */
-int mm_init(void) { return -1; }
+int mm_init(void) {
+  prol = (block_t*) mem_sbrk(2*TAGS_SIZE);
+  if (prol == (void *)-1) {
+    return -1;
+  }
+  flist_first = NULL;
+  block_set_size_and_allocated(prol, TAGS_SIZE, 1);
+  epil = block_next(prol);
+  block_set_size_and_allocated(epil, TAGS_SIZE, 1);
+  return 0;
+}
+
+static inline block_t *extend_heap() {
+  void* j = mem_sbrk(EXTENSION);
+  if (j == (void *) -1) {
+    return NULL;
+  }
+  block_set_size_and_allocated(epil, EXTENSION, 0);
+  j = epil;
+  insert_free_block(j);
+  epil = block_next(epil);
+  block_set_size_and_allocated(epil, TAGS_SIZE, 1);
+  return epil;
+}
+
+static inline int isbig(block_t *fb, size_t size) {
+  size_t bs = block_size(fb);
+  if (bs >= size) {
+    if (bs >= (size+MINBLOCKSIZE)) {
+      return 1;
+    }
+    return 0;
+  }
+  return -1;
+}
+
+static inline block_t *search(size_t size) {
+  block_t *fb = flist_first;
+  size_t leftover;
+  block_t *adjacent;
+
+  do
+  {
+    int j = isbig(fb, size);
+
+    if (j > -1) {
+      if (j == 1) {
+        //  ^^ free block has leftover space > MINBLOCKSPACE
+        leftover = block_size(fb) - size;
+        pull_free_block(fb);
+        block_set_size_and_allocated(fb, size, 1);
+        adjacent = block_next(fb);
+        block_set_size_and_allocated(adjacent, leftover, 0);
+        insert_free_block(adjacent);
+        return fb;
+      } else if (j == 0) {
+        //  ^^ free block does not have leftover space > MINBLOCKSPACE
+        pull_free_block(fb);
+        block_set_allocated(fb, 1);
+        return fb;
+      }
+    }
+    fb = block_flink(fb);
+  } while (fb != flist_first);
+  
+  return NULL;
+}
 
 /*     _ __ ___  _ __ ___      _ __ ___   __ _| | | ___   ___
  *    | '_ ` _ \| '_ ` _ \    | '_ ` _ \ / _` | | |/ _ \ / __|
@@ -53,9 +124,39 @@ int mm_init(void) { return -1; }
  */
 void *mm_malloc(size_t size) {
   // TODO
-
+  block_t *p;
+  size_t s = size+TAGS_SIZE;
+  if (size == 0) {
+    return NULL;
+  } else {
+      if (flist_first == NULL) {
+        p = extend_heap();
+        if (p == NULL) {
+          return NULL;
+        }
+      }
+      
+      if (s < MINBLOCKSIZE) {
+        s = MINBLOCKSIZE;
+      } else {
+        s = align(s);
+      }
+      block_t* fb = search(s);
+      if (fb == NULL) {
+        p = extend_heap();
+        if (p == NULL) {
+          return NULL;
+        }
+        fb = search(s);
+      }
+      block_t *payload = payload_to_block(fb);
+      fprintf(stderr, "payload address: %lx\n", (long unsigned) payload);
+      return payload;
+  }
   return NULL;
 }
+
+
 
 /*                              __
  *     _ __ ___  _ __ ___      / _|_ __ ___  ___
@@ -70,6 +171,7 @@ void *mm_malloc(size_t size) {
  */
 void mm_free(void *ptr) {
   // TODO
+  ptr = ptr;
 }
 
 /*
@@ -87,6 +189,7 @@ void mm_free(void *ptr) {
  */
 void *mm_realloc(void *ptr, size_t size) {
   // TODO
-
+  ptr = ptr;
+  size = size;
   return NULL;
 }
